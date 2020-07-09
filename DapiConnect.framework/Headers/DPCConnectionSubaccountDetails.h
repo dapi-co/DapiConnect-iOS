042cfb1d2a6bb85679ae23a77b20cf0e1c9aa304
Mohammed M. Ennabah <ennabahm9@gmail.com>
2020-07-09 09:53:47 +0400

    Subaccount details

---
+++ DapiConnect.framework/Headers/DPCConnectionSubaccountDetails.h
@@ -0,0 +1,23 @@
+//
+//  DPCConnectionSubaccountDetails.h
+//  DapiConnect
+//
+//  Created by Mohammed Ennabah on 07/07/2020.
+//  Copyright © 2020 Dapi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DPCConnectionSubaccountDetails : NSObject
+
+@property (nonatomic, copy) NSString *accountID;
+@property (nonatomic, copy) NSString *accountNumber;
+@property (nonatomic, copy) NSString *currencyCode;
+@property (nonatomic, copy) NSString *type;
+@property (assign) double balance;
+
+@end
+
+NS_ASSUME_NONNULL_END
